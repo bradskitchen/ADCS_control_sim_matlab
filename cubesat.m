c8a21397eb5a7bb6f6d39eea19fe7ae8c3567a32
Brayden <bwa0007@auburn.edu>
2025-05-17 15:54:47 -0500

    Fixed with Inertial Transformation Matrix

--- cubesat.m
+++ cubesat.m
@@ -1,6 +1,6 @@
 function dstate_dt = cubesat(t, state)
     %%state_0 = [x_0; y_0; z_0; vel_x_0; vel_y_0; vel_z_0];
-
+    global BX BY BZ
 
     %x = state(1);
     %y = state(2);
@@ -11,6 +11,8 @@ function dstate_dt = cubesat(t, state)
     %vel_y = state(5);
     %vel_z = state(6);
 
+    
+    
 
     %% inertia and mass
     m_cubesat = 4;
@@ -23,9 +25,29 @@ function dstate_dt = cubesat(t, state)
     %% gravity model
     [radius, m_earth, grav_const, mu] = earth();
     r = state(1:3);
-    r_norm = norm(r);
-    r_hat = r / r_norm;
-    F_grav = -(grav_const*m_earth*m_cubesat/r_norm^2) *r_hat;
+    rho = norm(r);
+    r_hat = r / rho;
+    F_grav = -(grav_const*m_earth*m_cubesat/rho^2) *r_hat;
+
+
+    %% call magfield model
+    phi_E = 0;
+    theta_E = acos(state(3) / rho);
+    psi_E = atan2(state(2), state(1));
+
+    latitude = 90 - theta_E * 180 / pi;
+    longitude = psi_E * 180 / pi;
+    altitude = (rho - radius)/1000; 
+   
+    [BXN, BYE, BZD] = igrf('15-may-2025', latitude, longitude, altitude, 'geocentric');
+
+
+    %% North, east, down, to X, Y, Z
+    B_NED = [BXN, BYE, BZD];
+    BI = inertial_transformation_matrix(phi_E, theta_E + pi, psi_E) .* B_NED;
+
+
+    
 
     %% dynamics
     F = F_grav;
