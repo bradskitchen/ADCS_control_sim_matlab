3a5860ae56e012f6873bd54069a9b6c6a6e40859
Brayden <bwa0007@auburn.edu>
2025-05-17 17:44:47 -0500

    Updated NED to XYZ conversion

--- cubesat.m
+++ cubesat.m
@@ -30,24 +30,27 @@ function dstate_dt = cubesat(t, state)
     F_grav = -(grav_const*m_earth*m_cubesat/rho^2) *r_hat;
 
 
-    %% call magfield model
+    %% X, Y, Z into Lat, Lon, Alt
     phi_E = 0;
     theta_E = acos(state(3) / rho);
     psi_E = atan2(state(2), state(1));
 
     latitude = 90 - theta_E * 180 / pi;
     longitude = psi_E * 180 / pi;
-    altitude = (rho - radius)/1000; 
-   
-    [BXN, BYE, BZD] = igrf('15-may-2025', latitude, longitude, altitude, 'geocentric');
+    altitude = (rho)/1000; %from center of earth in km
+    
+
+    %% Call mag field
+    [BXN, BYE, BZD] = igrf('01-jan-2020', latitude, longitude, altitude, 'geocentric');
 
 
     %% North, east, down, to X, Y, Z
-    B_NED = [BXN, BYE, BZD];
-    BI = inertial_transformation_matrix(phi_E, theta_E + pi, psi_E) .* B_NED;
+    B_NED = [BXN; BYE; -BZD]; %down is up
+    BI = inertial_transformation_matrix(phi_E, theta_E + pi, psi_E) * B_NED;
 
-
-    
+    BX = BI(1);
+    BY = BI(2);
+    BZ = BI(3);
 
     %% dynamics
     F = F_grav;
