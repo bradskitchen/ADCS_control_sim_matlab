04d08157de832be63718265570e1c5d87bbf6a43
Brayden <bwa0007@auburn.edu>
2025-05-17 17:49:32 -0500

    Fixed ITM

--- inertial_transformation_matrix.m
+++ inertial_transformation_matrix.m
@@ -1,12 +1,7 @@
 %% Inertial Transformation Matrix, matrix_T
 function T = inertial_transformation_matrix(alpha, beta, gamma) 
-
-    T = [(cos(alpha)*cos(beta)), ((cos(alpha)*sin(beta)*sin(gamma))-(sin(alpha)*cos(gamma))), ((cos(alpha)*sin(beta)*cos(gamma))+(sin(alpha)*sin(gamma)));
-     (sin(alpha)*cos(beta)), ((sin(alpha)*sin(beta)*sin(gamma))+(cos(alpha)*cos(gamma))), ((sin(alpha)*sin(beta)*cos(gamma))-(cos(alpha)*sin(gamma))); 
-     -(sin(beta)), (cos(beta)*sin(gamma)), (cos(beta)*cos(gamma))];
-
-
-
-
-
+    %p,t,s -> a,b,g
+    T = [(cos(beta)*cos(gamma)), ((sin(alpha)*sin(beta)*cos(gamma))-(cos(alpha)*sin(gamma))), ((cos(alpha)*sin(beta)*cos(gamma))+(sin(alpha)*sin(gamma)));
+     (cos(beta)*sin(gamma)), ((sin(alpha)*sin(beta)*sin(gamma))+(cos(alpha)*cos(gamma))), ((cos(alpha)*sin(beta)*sin(gamma))-(sin(alpha)*cos(gamma))); 
+     -(sin(beta)), (sin(alpha)*cos(beta)), (cos(alpha)*cos(beta))];
 end
